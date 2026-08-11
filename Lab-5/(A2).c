40a54f0f7608e66f626f9d9d916a013641d790f5
PrathamGhiya <ghiya.pratham@gmail.com>
2026-08-11 18:41:27 +0530

    Create (A2).c

---
+++ Lab-5/(A2).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+    // With temp var
+    int a ,b, temp;
+    printf("\nEnter 1st no.: ");
+    scanf("%d",&a);
+    printf("\nEnter 2nd no.: ");
+    scanf("%d",&b);
+    printf("\nBefore swapping: a = %d and b = %d",a,b);
+    if(a!=b){
+        temp = a;
+        a = b;
+        b = temp;
+        printf("\nAfter swapping: a = %d and b = %d",a,b);
+    }
+    else{
+        printf("\nBoth are same");
+        printf("\nThus a = %d and b = %d",a,b);
+    }
+}
