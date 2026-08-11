6abc9ad2c97b9cc57cb5e2b896b08c9be85a3516
PrathamGhiya <ghiya.pratham@gmail.com>
2026-08-11 18:45:11 +0530

    Update (A2).c

--- Lab-5/(A2).c
+++ Lab-5/(A2).c
@@ -2,16 +2,32 @@
 int main(){
 
     // With temp var
-    int a ,b, temp;
+    // int a ,b, temp;
+    // printf("\nEnter 1st no.: ");
+    // scanf("%d",&a);
+    // printf("\nEnter 2nd no.: ");
+    // scanf("%d",&b);
+    // printf("\nBefore swapping: a = %d and b = %d",a,b);
+    // if(a!=b){
+    //     temp = a;
+    //     a = b;
+    //     b = temp;
+    //     printf("\nAfter swapping: a = %d and b = %d",a,b);
+    // }
+    // else{
+    //     printf("\nBoth are same");
+    //     printf("\nThus a = %d and b = %d",a,b);
+    // }
+
+    // Witout temp var
+    int a ,b;
     printf("\nEnter 1st no.: ");
     scanf("%d",&a);
     printf("\nEnter 2nd no.: ");
     scanf("%d",&b);
     printf("\nBefore swapping: a = %d and b = %d",a,b);
     if(a!=b){
-        temp = a;
-        a = b;
-        b = temp;
+        a = (a+b) - (b=a);
         printf("\nAfter swapping: a = %d and b = %d",a,b);
     }
     else{
