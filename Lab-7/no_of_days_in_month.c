65851c00de12ad15d8240894a64b6426c5faa5a9
PrathamGhiya <ghiya.pratham@gmail.com>
2026-08-15 12:10:36 +0530

    Create no of days in month

---
+++ Lab-7/no_of_days_in_month.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+int main(){
+    int choice;
+    printf("Enter your choice: ");
+    scanf("%d",&choice);
+    switch(choice){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            printf("31 Days");
+            break;
+
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            printf("30 Days");
+            break;
+        
+        case 2:
+            printf("28 Days");
+            break;
+
+        default:
+            printf("Invalid Choice");
+            break;
+    }
+}
