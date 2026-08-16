93611c458d1533bbc6e7c65d19cc72a26706c9e8
PrathamGhiya <ghiya.pratham@gmail.com>
2026-08-16 10:10:47 +0530

    Create multiply largest among 2 with 3rd

---
+++ Lab-7/multiply_largest_among_2_with_3rd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter 1st no.: ");
+    scanf("%d",&a);
+    printf("Enter 2nd no.: ");
+    scanf("%d",&b);
+    printf("Enter 3rd no.: ");
+    scanf("%d",&c);
+    printf("%d",(a>b) ? (a*c) : (b*c));
+}
