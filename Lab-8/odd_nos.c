011fde2015a705bf8ab413c3a69358ac55fd0791
PrathamGhiya <ghiya.pratham@gmail.com>
2026-08-20 10:13:08 +0530

    Create odd numbers by while program

---
+++ Lab-8/odd_nos.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+int main(){
+    int n, i=1;
+    printf("Enter a no.: ");
+    scanf("%d",&n);
+
+    // while(i<=n){
+    //     printf("\n%d",i);
+    //     i+=2;
+    // }
+
+    // OR
+
+    // while(i<=n/2){
+    //     printf("\n%d",(i*2)-1);
+    //     i++;
+    // }
+    
+
+
+    // do{
+    //     printf("\n%d",i);
+    //     i+=2;
+    // }while(i<=n);
+
+    // OR
+
+    // do{
+    //     printf("\n%d",(i*2)-1);
+    //     i++;
+    // }while(i<=n/2);
+}
