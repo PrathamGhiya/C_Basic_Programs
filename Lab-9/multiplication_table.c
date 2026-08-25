5b1556cf4fed3a578f8868e193a1828e397be1c8
PrathamGhiya <ghiya.pratham@gmail.com>
2026-08-25 21:20:05 +0530

    Create multiplication table

---
+++ Lab-9/multiplication_table.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int n,i=1;
+    printf("Enter a no.: ");
+    scanf("%d",&n);
+    while(i<=10){
+        printf("\n%d * %d = %d",n,i,n*i);
+        i++;
+    }
+}
