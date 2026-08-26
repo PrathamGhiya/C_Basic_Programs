60449ef77019319827358888eb844ed776cc937d
PrathamGhiya <ghiya.pratham@gmail.com>
2026-08-26 20:34:10 +0530

    Create uppercase and lowercase alphabet

---
+++ uppercase_and_lowercase_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    char i=65;
+    while(i<=90){
+        printf("%c ",i);
+        i++;
+    }
+    i=97;
+    printf("\n");
+    while(i<=122){
+        printf("%c ",i);
+        i++;
+    }
+}
