6f266d0f6672f08904e2889cd3a14223c05d434e
PrathamGhiya <ghiya.pratham@gmail.com>
2026-08-27 23:06:04 +0530

    Create reverse order

---
+++ Lab-10/reverse_order.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+int main(){
+    int n, rev=0;
+    printf("Enter a no.: ");
+    scanf("%d",&n);
+    while(n!=0){
+        rev = (rev*10) + (n%10);
+        n/=10;
+    }
+    printf("%d",rev);
+}
